6f2ded8848e40eb4940e1634e01b07c7d8d17edb
KOKOS2003 <macs.shvets03.2016@gmail.com>
2024-04-11 02:41:11 +0300

    add change

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ click.addEventListener('click',()=>{
     if(ol.style.display == "flex"){
         ol.style.display = "none"
         document.querySelector('.container').style.height = "auto"
-        document.querySelector('.header').style.height = 60 +'vh'
+        document.querySelector('.header').style.height = 70+'vh'
         burger.style.background = "rgb(0,0,0,0.0001)"
         document.querySelector('.burger__1').style.opacity = 1
         document.querySelector('.burger__2').style.opacity = 1
@@ -21,7 +21,7 @@ click.addEventListener('click',()=>{
 })
 li1.forEach((item) => {
     item.addEventListener('click', () => {
-        document.querySelector('.header').style.height = 60+'vh';
+        document.querySelector('.header').style.height = 'auto';
         document.querySelector('.container').style.height = 'auto';
         console.log("hello");
         ol.style.display = "none"
@@ -31,20 +31,45 @@ li1.forEach((item) => {
     });
 });
 
+const clickCircle = document.querySelectorAll('.clickCircle');
+
+clickCircle.forEach((item) => {
+    item.addEventListener('click', () => {
+        const currentStatus = item.getAttribute('data-status');
+        if (currentStatus === "empty") {
+            item.src = '/img/sircleXl.png';
+            item.setAttribute('data-status', 'filled');
+        } else {
+            item.src = "/img/sircleEmptyXl.png";
+            item.setAttribute('data-status', 'empty');
+        }
+    });
+});
 
 
 
 
-// buttonBaby.addEventListener('click' , () => {
-//     buttonBaby.style.background = 'black';
-//     buttonTeen.style.background = 'white';
-//     buttonBaby.style.color = 'white';
-//     buttonTeen.style.color = 'black';
-// });
-// buttonTeen.style.background = 'black';
-// buttonTeen.style.color = 'white';
-// buttonBaby.style.background = 'white';
-// buttonBaby.style.color = 'black';
+const buttonBaby = document.querySelector('.baby__button');
+const buttonTeen = document.querySelector('.tenn__button');
+
+function toggleButton(activeButton, inactiveButton) {
+    activeButton.style.background = 'black';
+    activeButton.style.color = 'white';
+    inactiveButton.style.background = 'white';
+    inactiveButton.style.color = 'black';
+}
+
+buttonBaby.addEventListener('click', () => {
+    toggleButton(buttonBaby, buttonTeen);
+});
+
+buttonTeen.addEventListener('click', () => {
+    toggleButton(buttonTeen, buttonBaby);
+});
+
+// Встановлення початкових стилів
+toggleButton(buttonTeen, buttonBaby);
+
 
 // const parent =  document.querySelectorAll('.block3__slide_subtext2');
 // const circles = parent.querySelectorAll('.clickCircle');
